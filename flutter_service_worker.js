4b6f24183f96e061b9a0f6fb523ce116f804782f
irahulcse <irahulcse@users.noreply.github.com>
2020-04-02 19:50:10 +0000

    deploy: ca291242b06a1eb1693c8f67a21eb37cc4d8e4cd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
-"/main.dart.js": "5c3f833b8960274b76932afded39d456",
+"/main.dart.js": "430ba090e336a99b8b20808fd0acf7d2",
 "/index.html": "895b79313f2c0cd5e8cfed1371060120"
 };
 
