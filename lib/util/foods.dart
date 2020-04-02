6f87c6aa3a6ef09526cd85c36126ff95107a1a60
irahulcse <1rahulchandra1@gmail.com>
2020-04-03 00:50:27 +0530

    Come

--- lib/util/foods.dart
+++ lib/util/foods.dart
@@ -1,26 +1,26 @@
 List<Map> foods = [
   {
     "img": "assets/hotel3.jpg",
-    "name": "Fruit Salad"
+    "name": "Hotel Milan"
   },
   {
     "img": "assets/hotel4.jpg",
-    "name": "Fruit Salad"
+    "name": "Hotel Sultan"
   },
   {
     "img": "assets/hotel6.jpg",
-    "name": "Hamburger"
+    "name": "Paradise Hotel"
   },
   {
     "img": "assets/hotel7.jpg",
-    "name": "Fruit Salad"
+    "name": "Never Say Never Motel"
   },
   {
     "img": "assets/hotel9.jpg",
-    "name": "Hamburger"
+    "name": "Quick Bite Hotels"
   },
   {
     "img": "assets/hotel11.jpg",
-    "name": "Steak"
+    "name": "Kick Me Nice"
   },
 ];
