71f5b0fa0012110157f59e439de6e222f5b69378
irahulcse <1rahulchandra1@gmail.com>
2020-04-03 17:13:22 +0530

    Happen Sometimes

--- lib/util/foods.dart
+++ lib/util/foods.dart
@@ -2,43 +2,43 @@ List<Map> foods = [
   {
     "img": "assets/hotel3.jpg",
     "name": "Hotel Milan",
-    "reviews": "5.0 (23 Reviews)",
-    "pieces": "10 Pieces",
+    "reviews": "5.0 (33 Reviews)",
+    "pieces": "23 Pieces",
     "description": "Sky is Blue" " Drinks are Red" "Hotel3 who are you"
   },
   {
     "img": "assets/hotel4.jpg",
     "name": "Hotel Sultan",
-    "reviews": "5.0 (23 Reviews)",
-    "pieces": "10 Pieces",
+    "reviews": "5.0 (99 Reviews)",
+    "pieces": "567 Pieces",
     "description": "Sky is Blue" " Drinks are Red" "Hotel4 who are you"
   },
   {
     "img": "assets/hotel6.jpg",
     "name": "Paradise Hotel",
-    "reviews": "5.0 (23 Reviews)",
-    "pieces": "10 Pieces",
+    "reviews": "5.0 (17 Reviews)",
+    "pieces": "101 Pieces",
     "description": "Sky is Blue" " Drinks are Red" "Hotel6 who are you"
   },
   {
     "img": "assets/hotel7.jpg",
     "name": "Never Say Never Motel",
-    "reviews": "5.0 (23 Reviews)",
-    "pieces": "10 Pieces",
+    "reviews": "5.0 (12 Reviews)",
+    "pieces": "65 Pieces",
     "description": "Sky is Blue" " Drinks are Red" "Hotel7 who are you"
   },
   {
     "img": "assets/hotel9.jpg",
     "name": "Quick Bite Hotels",
-    "reviews": "5.0 (23 Reviews)",
-    "pieces": "10 Pieces",
+    "reviews": "5.0 (18 Reviews)",
+    "pieces": "19 Pieces",
     "description": "Sky is Blue" " Drinks are Red" "Hotel 9 who are you"
   },
   {
     "img": "assets/hotel11.jpg",
     "name": "Kick Me Nice",
-    "reviews": "5.0 (23 Reviews)",
-    "pieces": "10 Pieces",
+    "reviews": "5.0 (12 Reviews)",
+    "pieces": "6 Pieces",
     "description": "Sky is Blue" " Drinks are Red" "Hotel11 who are you"
   },
 ];
